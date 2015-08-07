5939808c3c62ea7df6eeaf1729ca8b916d09335f
Paul Mitchum <paul@mile23.com>
2015-08-06 20:28:02 -0600

    mostly working

--- src/Command/VerifyCommand.php
+++ src/Command/VerifyCommand.php
@@ -40,26 +40,23 @@ class VerifyCommand extends Command implements ContainerAwareInterface {
     $output->writeln('Crawling: ' . $url);
     $sitemap = new SitemapCrawler($url);
 
-    $resources = array();
+    $bad_urls = [];
 
     $p = new ProgressBar($output, count($sitemap));
-    $p->setMessage('Crawling...');
     $p->start();
-
     $client = new Client();
-    $client->getClient()->setDefaultOption('config/curl/' . CURLOPT_TIMEOUT, 2);
-
+    $client->getClient()->setDefaultOption('config/curl/' . CURLOPT_TIMEOUT, 5);
     // Pull in all URLs from the sitemap file(s), and compile a list of linked
     // URLs to check.
-    // Linked array has the URL as key and the HTTP HEAD status code as item.
+    // Linked array has the URL as key and NULL as the value, to be filled in
+    // later with the status code of a HEAD request.
     $linked = array();
     foreach ($sitemap as $page_url) {
 //      \sleep(2);
       $crawler = $client->request('GET', $page_url);
-
       $status = $client->getResponse()->getStatus();
       if ($status != 200) {
-        $resources[] = $page_url;
+        $bad_urls[] = $page_url;
       }
       else {
         $page_crawler = new HtmlCrawler($crawler, new UrlBuilder('', $base_url));
@@ -69,18 +66,36 @@ class VerifyCommand extends Command implements ContainerAwareInterface {
       }
       $p->advance();
     }
-
     $p->finish();
 
+    $linked_urls = [];
+    $output->writeln('');
+    $output->writeln('Checking outbound links...');
+    $p = new ProgressBar($output, count($linked));
+    $p->start();
     // Verify all linked URLs.
-    foreach($linked as $resource_url => $status) {
-      $crawler = $client->request('HEAD', $resource_url);
-      $linked[$resource_url] = $client->getResponse()->getStatus();
+    foreach($linked as $resource_url => $foo) {
+      try {
+        $crawler = $client->request('HEAD', $resource_url);
+        $status = $client->getResponse()->getStatus();
+        if ($status < 400) {
+          $linked_urls[$resource_url] = $client->getResponse()->getStatus();
+        }
+        else {
+          $bad_urls[] = $resource_url;
+        }
+      }
+      // @todo: change this to a more specific exception for bad DNS.
+      catch (\Exception $e) {
+        $bad_urls[] = $resource_url;
+      }
+      $p->advance();
     }
+    $p->finish();
 
-    error_log(print_r($linked, TRUE));
+//    error_log(print_r($linked_urls, TRUE));
 
-    foreach ($resources as $item) {
+    foreach ($bad_urls as $item) {
       $output->writeln($item);
     }
     $output->writeln('');
